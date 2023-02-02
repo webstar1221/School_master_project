127f9d210e8dbe709807f138132ccb548c4a4e0f
webstar1221 <djangostars1221@gmail.com>
2023-02-02 18:41:51 +0200

    this is completed project

---
+++ app/Helpers/Interfaces/PointTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Helpers\Interfaces;
+
+interface PointTypes
+{
+    const SCHOOL_EVENTS = 1;
+    const UNIVERSITY_EVENTS = 2;
+    const STUDENT_ACTIVITY = 3;
+
+    const ENROLLMENT_CREDIT = 4;
+}
